0a92556127757ae63efd972f98e6855825721920
Bigmagno7 <167204543+Bigmagno7@users.noreply.github.com>
2025-09-07 18:19:46 +0200

    Add files via upload

---
+++ Filiera_Magnoni_Morganti_Scarpini/src/main/java/unicam/piattaforma_filiera_agricola/controller/AnimatoreController.java
@@ -0,0 +1,43 @@
+package unicam.piattaforma_filiera_agricola.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import unicam.progettofiliera.models.eventi.Evento;
+import unicam.progettofiliera.service.AnimatoreHandler;
+
+@RestController
+@RequestMapping("/bachecaEventi")
+public class AnimatoreController {
+
+    private final AnimatoreHandler animatoreHandler;
+
+    @Autowired
+    public AnimatoreController(AnimatoreHandler animatoreHandler) {
+        this.animatoreHandler = animatoreHandler;
+    }
+
+    @PostMapping("/{idAnimatore}/add")
+    public ResponseEntity<String> pubblicaEvento(@PathVariable Long idAnimatore,
+                                                 @RequestBody Evento evento){
+
+        animatoreHandler.caricaEvento(
+                idAnimatore,
+                evento.getNome(),
+                evento.getLuogo(),
+                evento.getDescrizione(),
+                evento.getMaxPartecipanti()
+        );
+
+        return ResponseEntity.ok("Evento aggiunto");
+    }
+
+    @DeleteMapping("/{idAnimatore}/delete/{idEvento}")
+    public ResponseEntity<String> eliminaEvento(@PathVariable Long idAnimatore,
+                                                @PathVariable Long idEvento) {
+
+        animatoreHandler.deleteEvento(idAnimatore,idEvento);
+        return ResponseEntity.ok("Evento cancellato");
+    }
+}
